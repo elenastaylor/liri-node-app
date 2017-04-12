e1ce330cb2781d7e3b10c535a850b16f9dda1d36
elenastaylor <elena.staylor@gmail.com>
2017-04-11 23:03:10 -0400

    created files for node app

---
+++ keys.js
@@ -0,0 +1,15 @@
+/**
+ * Created by elenastaylor on 4/8/17.
+ */
+console.log('this is loaded');
+
+exports.twitterKeys = {
+    consumer_key: 'LCF3a0ZxZIFXCUFcLpNzrExko',
+    consumer_secret: 'k96AZ6MVHYOVK6Cj20dM5BZBvw1ws4MZOLKcxLOBJfTbayzIQu',
+    access_token_key: '850886949221130241-C4JLv0dRRN3SIBrnwrzQ3m4vDzgfDy9',
+    access_token_secret: 'gd1ykIxeEqIDeJm1c9Mv9FPXYCCEMLaLhr5nsOrYVLhuJ'
+}
+//Consumer Key (API Key)	LCF3a0ZxZIFXCUFcLpNzrExko
+//Consumer Secret (API Secret)	k96AZ6MVHYOVK6Cj20dM5BZBvw1ws4MZOLKcxLOBJfTbayzIQu
+//Access Token	850886949221130241-C4JLv0dRRN3SIBrnwrzQ3m4vDzgfDy9
+//Access Token Secret	gd1ykIxeEqIDeJm1c9Mv9FPXYCCEMLaLhr5nsOrYVLhuJ
